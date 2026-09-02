0792c83631fd8e1c3482cab3cc27c0edc0400592
jar360 <janeadelinar@gmail.com>
2026-09-02 21:52:18 +0530

    Updated package-lock.json

--- frontend/dist/assets/index-DOm8uuhF.js
+++ frontend/dist/assets/index-DOm8uuhF.js
@@ -109,7 +109,7 @@ Error generating stack: `+i.message+`
  *
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
- */const qc=ue("FilePlus2",[["path",{d:"M4 22h14a2 2 0 0 0 2-2V7l-5-5H6a2 2 0 0 0-2 2v4",key:"1pf5j1"}],["path",{d:"M14 2v4a2 2 0 0 0 2 2h4",key:"tnqrlb"}],["path",{d:"M3 15h6",key:"4e2qda"}],["path",{d:"M6 12v6",key:"1u72j0"}]]);/**
+ */const qc=ue("FilePlus",[["path",{d:"M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z",key:"1rqfz7"}],["path",{d:"M14 2v4a2 2 0 0 0 2 2h4",key:"tnqrlb"}],["path",{d:"M9 15h6",key:"cctwl0"}],["path",{d:"M12 18v-6",key:"17g6i2"}]]);/**
  * @license lucide-react v0.456.0 - ISC
  *
  * This source code is licensed under the ISC license.
